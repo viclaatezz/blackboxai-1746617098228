762e640653508d2db105fb48e6a75a4beef7705d
viclaatezz <hysteria1008@gmail.com>
2025-05-07 18:24:43 +0700

    Add backend/application/views/watchlists/edit.php 1746617127626

---
+++ backend/application/views/watchlists/edit.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit Watchlist</title>
+</head>
+<body>
+    <h1>Edit Watchlist</h1>
+    <?php echo validation_errors(); ?>
+    <?php echo form_open('watchlists/update/'.$watchlist['id']); ?>
+        <label for="portfolio_id">Portfolio:</label>
+        <select name="portfolio_id" id="portfolio_id">
+            <?php foreach ($portfolios as $portfolio): ?>
+                <option value="<?php echo $portfolio['id']; ?>" <?php echo set_select('portfolio_id', $portfolio['id'], ($watchlist['portfolio_id'] == $portfolio['id'])); ?>>
+                    <?php echo htmlspecialchars($portfolio['name'], ENT_QUOTES, 'UTF-8'); ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <br>
+
+        <label for="coin_id">Coin:</label>
+        <select name="coin_id" id="coin_id">
+            <?php foreach ($coins as $coin): ?>
+                <option value="<?php echo $coin['id']; ?>" <?php echo set_select('coin_id', $coin['id'], ($watchlist['coin_id'] == $coin['id'])); ?>>
+                    <?php echo htmlspecialchars($coin['symbol'], ENT_QUOTES, 'UTF-8'); ?> - <?php echo htmlspecialchars($coin['name'], ENT_QUOTES, 'UTF-8'); ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <br>
+
+        <input type="submit" value="Update">
+    <?php echo form_close(); ?>
+    <a href="<?php echo site_url('watchlists'); ?>">Back to Watchlists</a>
+</body>
+</html>
